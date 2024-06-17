ef57573c96b00b97021c3df3d9f5b67d41195022
Xingzhe Li <xngzhli@gmail.com>
2024-06-17 13:52:33 -0600

    print

--- stylesan-xl/training/training_loop.py
+++ stylesan-xl/training/training_loop.py
@@ -212,6 +212,7 @@ def training_loop(
         z = torch.empty([batch_gpu, G.z_dim], device=device)
         c = torch.empty([batch_gpu, G.c_dim], device=device)
         img = misc.print_module_summary(G, [z, c])
+        print(img.shape)
         misc.print_module_summary(D, [img, c])
 
     # Setup augmentation.
