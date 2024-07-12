9f49bcec22601f6e2bc5d4a7f725ecc8504f2f36
Xingzhe Li <xngzhli@gmail.com>
2024-07-12 09:39:35 -0600

    debug

--- stylesan-xl/metrics/metric_utils.py
+++ stylesan-xl/metrics/metric_utils.py
@@ -61,6 +61,7 @@ def get_feature_detector(url, device=torch.device('cpu'), num_gpus=1, rank=0, ve
         if not is_leader and num_gpus > 1:
             torch.distributed.barrier() # leader goes first
         with dnnlib.util.open_url(url, verbose=(verbose and is_leader)) as f:
+            print(url)
             model = torch.load(f, map_location=device)
             if custom:
                 _feature_detector_cache[key] = lambda *a : model(*a).pooler_output
